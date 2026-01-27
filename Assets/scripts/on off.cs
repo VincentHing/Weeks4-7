e0258b4321fab8fea10f8936026295d801cb712f
hingley <hingley@OA-S141-25>
2026-01-27 11:15:52 -0500

    code gym

---
+++ Assets/scripts/on off.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class onoff : MonoBehaviour
+{
+    SpriteRenderer aliem;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//public void On()
+//{
+   
+//}
+//public void Off()
+//{
+
+//}
