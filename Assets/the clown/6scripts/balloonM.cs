cd1be5aca07890eb490b63d905ed4a9b1400848a
hingley <hingley@OA-S141-25>
2026-02-10 11:57:04 -0500

    almost finished the baloon movement

--- Assets/the clown/6scripts/balloonM.cs
+++ Assets/the clown/6scripts/balloonM.cs
@@ -1,7 +1,17 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class balloonM : MonoBehaviour
 {
+    //animation curve setup
+    public AnimationCurve wiggly;
+    Vector2 bloonp;
+    //end when off screen
+    Vector2 startpos = new Vector2(3.7f, -1);
+    Vector2 endpos = new Vector2(3.7f, 6);
+    public float clock;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,12 @@ public class balloonM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        clock += Time.deltaTime;
+
+        bloonp = transform.position;
+        //multiply for 50% more speed
+        bloonp.y = Mathf.Lerp(startpos.y, endpos.y, clock/4);
+        transform.position = bloonp;
         
     }
 }
