6ebf47bf2fca27d64f2ac7a35e0d00c913c01323
hingley <hingley@OA-S143-04>
2026-02-13 13:02:42 -0500

    finally finished the baloon movement

--- Assets/the clown/6scripts/balloonM.cs
+++ Assets/the clown/6scripts/balloonM.cs
@@ -8,9 +8,11 @@ public class balloonM : MonoBehaviour
     public AnimationCurve wiggly;
     Vector2 bloonp;
     //end when off screen
-    Vector2 startpos = new Vector2(3.7f, -1);
-    Vector2 endpos = new Vector2(3.7f, 6);
+    Vector2 startpos = new Vector2(3.78f, -1);
+    Vector2 endpos = new Vector2(3.78f, 6);
     public float clock;
+    //looping value between 0 and 1 for the animation curve.
+    public float repClock;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,10 +24,18 @@ public class balloonM : MonoBehaviour
     void Update()
     {
         clock += Time.deltaTime;
+        //setting up the 2 timers
+        repClock += Time.deltaTime;
+        if (repClock >= 1)
+        {
+            repClock = 0;
+        }
 
         bloonp = transform.position;
         //multiply for 50% more speed
         bloonp.y = Mathf.Lerp(startpos.y, endpos.y, clock/4);
+        //gives the baloon a modifier to it's current x based on the animation curve, this was more difficult that it should've been
+        bloonp.x = 3.78f + wiggly.Evaluate(repClock);
         transform.position = bloonp;
         
     }
